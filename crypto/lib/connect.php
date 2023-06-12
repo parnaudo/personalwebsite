2196cb841ce5f9d9ceb7dba6a18b632b4263579c
Paul Arnaudo <paularnaudo@kl170.lan>
2023-06-12 13:39:37 -0700

    first rev

--- crypto/lib/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "db_config.php";
-$mysqli = new mysqli(HOST, USER, PW, DATABASE);
-if ($mysqli->connect_errno) {
-	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-?>
