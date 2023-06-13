5c9832c8e322330ddc4f2c1ce13f73f428531cbb
Paul Arnaudo <paularnaudo@kl170.lan>
2023-06-13 10:12:26 -0700

    no moar backticks

--- bash_cache_thrash.sh
+++ bash_cache_thrash.sh
@@ -1,6 +1,6 @@
 aws s3 sync . s3://www.arnaudo.info/ --exclude ".git/*"
 echo "syncing s3"
-for i in `aws cloudfront list-distributions | jq '[.[] | .Items[].Id ]' | awk -F '"' '{print $2}' `
+for i in $(aws cloudfront list-distributions | jq '[.[] | .Items[].Id ]' | awk -F '"' '{print $2}')
 do 
     aws cloudfront create-invalidation --distribution-id=`echo $i` --paths / > /dev/null 
     echo "clearing cache $i"
